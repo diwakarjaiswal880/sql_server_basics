22c68d0a8c2d09dcab4a477b29098dcd742ddb5f
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 12:14:55 +0530

    Add files via upload

---
+++ LINQ/LINQOrderByOperatorExample/LINQOrderByOperatorExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQOrderByOperatorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> Objstudent = new List<Student>(){
+            new Student() { RoleNumberId=1, Name = "Ak", Gender = "Male", Subjects = new List<string> { "Mathematics", "Physics" } },
+                new Student() { RoleNumberId=2, Name = "Shalu", Gender = "Female", Subjects = new List<string> { "Computers", "Botany" } },
+                new Student() { RoleNumberId=3, Name = "Shubham", Gender = "Male", Subjects = new List<string> { "Economics", "Operating System", "Java" } },
+                new Student() { RoleNumberId=4, Name = "Shubham", Gender = "Male", Subjects = new List<string> { "Accounting", "Social Studies", "Chemistry" } },
+                new Student() { RoleNumberId=5, Name = "Shivani", Gender = "FeMale", Subjects = new List<string> { "English", "Charterd" } }
+            };
+
+            var studentname = Objstudent.OrderBy(x => x.Name);
+
+            Console.WriteLine("Name of the student in the ascending form");
+            foreach (var student in studentname)  
+            {
+                Console.WriteLine(student.Name);
+            }
+
+            /*OrderByDescending() operator is used to print  
+             the name of the student in the descending form*/
+
+            studentname = Objstudent.OrderByDescending(x => x.Name);
+            Console.WriteLine("\nName of the student in the descending form");
+            //foreach loop is used to print the name of the student  
+            foreach (var student in studentname)
+            {
+                Console.WriteLine(student.Name);
+            }
+
+            //ThenBy() operator is used here to sort the Information of the student in ascending form on the behalf of the RollNumber  
+            studentname = Objstudent.OrderBy(x => x.Name).ThenBy(x => x.RoleNumberId);
+
+            Console.WriteLine("\nName of the student in the ascending form on the behalf of the Name and RollNumber");
+            //foreach loop is used to print the information  
+            foreach (var student in studentname)
+            {
+                Console.WriteLine("Name = {0} studentid = {1}", student.Name, student.RoleNumberId);
+            }
+
+            //ThenByDescending() operator is used to sort the information of the student in the descending form  
+            studentname = Objstudent.OrderBy(x => x.Name).ThenByDescending(x => x.RoleNumberId);
+
+            Console.WriteLine("\nName of the student in the descending form on the behalf of the RollNumber");
+
+            foreach (var student in studentname)
+            {
+                Console.WriteLine("Name={0} StudentId={1}", student.Name, student.RoleNumberId);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    class Student
+    {
+        public int RoleNumberId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public List<string> Subjects { get; set; }
+    }
+}
---
+++ LINQ/LINQTakeOperator/LINQTakeOperator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQTakeOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an array countries of string type with the initialized array  
+            string[] countries = { "India", "Ireland", "USA", "Russia", "China", "Australia", "Argentina", "Indonesia" };
+
+            //The Example of LINQ Take Operator in Method Syntax
+
+            //Here take() method will return the value from the String array upto three numbers.  
+            IEnumerable<string> result = countries.Take(3);
+            Console.WriteLine("The Example of LINQ Take Operator in Method Syntax");
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+            //The Example of LINQ Take () Operator in Query Syntax
+            IEnumerable<string> result1 = (from x in countries select x).Take(3);
+            Console.WriteLine("\nThe Example of LINQ Take () Operator in Query Syntax");
+            foreach (string s in result1)
+            {
+                Console.WriteLine(s);
+            }
+
+            //Example of LINQ TakeWhile in Method Syntax
+
+            /*TakeWhile operator is used which will print  
+              the values until the specified condition is satisfied.*/
+            IEnumerable<string> result2 = countries.TakeWhile(x => x.StartsWith("I"));
+
+            Console.WriteLine("\nExample of LINQ TakeWhile in Method Syntax");
+            //foreach loop will print the value of the result  
+            foreach (string s in result2)
+            {
+                Console.WriteLine(s);
+            }
+
+            //Example of LINQ TakeWhile in Query Syntax
+
+            //apply the query syntax to print the values upto the specified condition.StartWith("A").  
+            IEnumerable<string> result3 = (from x in countries select x).TakeWhile(x => x.StartsWith("I"));
+            Console.WriteLine("\nExample of LINQ TakeWhile in Query Syntax");
+            foreach (string s in result3)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
